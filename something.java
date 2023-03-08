b9410f7a5264c13e7c087ae7d4785ea0653ea1a9
Zachary Leach <leachzj@appstate.edu>
2023-03-08 18:55:26 -0500

    Completed the assignment

--- something.java
+++ something.java
@@ -1,4 +1,8 @@
 
+/**
+ * This is just some code to submit along with the readme. Don't mind this, it doesn't do anything
+ * relevant to the assignment.
+ */
 public class something {
     public static void main(String args[]) {
         System.out.print("Hello World!");
